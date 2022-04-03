c2be2e8c9e787900f7b65acb614dee8d45b60341
Charles Trangay <charles@nbp-175-222.nbp.ruw.rutgers.edu>
2022-04-03 16:08:14 -0400

    page5 changed

---
+++ screens/page5/Page5.styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#5D8AA8',
+    },
+    title: {
+      fontSize: 40,
+      fontWeight: 'bold',
+    },
+    textInput: {
+      borderWidth: 1,
+      padding: 5,
+      height: 100,
+    },
+    content: {
+      
+    }
+  });
--- screens/page5/Page5.tsx
+++ screens/page5/Page5.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-import {View} from 'react-native'
+import React from "react";
+import { View, Text, TextInput } from "react-native";
+import { styles } from "./Page5.styles";
+
 export default function page5() {
-  return (
-    <View>
-        PAGE 5
+	return (
+		<View style={styles.container}>
+      <View style = {styles.content}>
+			<Text style={styles.title}>Enter text to send:</Text>
+			<TextInput style = {styles.textInput} multiline numberOfLines={4}></TextInput>
+      </View>
     </View>
-  )
+	);
 }
