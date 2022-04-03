4a16f0ee1778d1956a3cdaace634149b6568b10b
Charles Trangay <charles@nbp-175-222.nbp.ruw.rutgers.edu>
2022-04-03 16:32:00 -0400

    minor page5 changes

--- screens/page5/Page5.tsx
+++ screens/page5/Page5.tsx
@@ -1,14 +1,24 @@
 import React from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./Page5.styles";
 
-export default function page5() {
+export default function page5( {navigation}: {navigation: any} ) {
 	return (
 		<View style={styles.container}>
-      <View style = {styles.content}>
-			<Text style={styles.title}>Enter text to send:</Text>
-			<TextInput style = {styles.textInput} multiline numberOfLines={4}></TextInput>
-      </View>
-    </View>
+			<View style={styles.content}>
+				<Text style={styles.title}>Enter text to send:</Text>
+				<TextInput
+					style={styles.textInput}
+					multiline
+					numberOfLines={4}
+				></TextInput>
+			</View>
+      <TouchableOpacity
+					key={1}
+					onPress={() => navigation.navigate("tabTwoScreen")}
+				>
+					<Text style={styles.button}>Next page </Text>
+				</TouchableOpacity>
+		</View>
 	);
 }
