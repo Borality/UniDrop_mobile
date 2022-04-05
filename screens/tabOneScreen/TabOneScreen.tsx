18dbfb3f26a3017f0ad7bd5e56a0e9b618ba3416
Charles Trangay <charles@nbp-142-83.nbp.ruw.rutgers.edu>
2022-04-05 18:25:54 -0400

    fixed buttons

--- screens/tabOneScreen/TabOneScreen.tsx
+++ screens/tabOneScreen/TabOneScreen.tsx
@@ -45,6 +45,8 @@ export default function TabOneScreen({ navigation }: { navigation: any }) {
 				</View>
 
 				{/*Add pages here */}
+        <MyButton title = "Page 1" page="StartScreen" navigation={navigation}/>
+        <MyButton title = "Page 2" page="RoomID" navigation={navigation}/>
 				<MyButton title="Page 5" page="page5" navigation={navigation} />
 				<MyButton title="Page 6" page="page6" navigation={navigation} />
 				<MyButton title="Page 7" page="page7" navigation={navigation} />
