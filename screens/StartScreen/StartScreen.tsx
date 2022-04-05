d83a51cc55177b5e5797fbb0af725dee72352687
Tyron Tucker <tlt88@scarletmail.rutgers.edu>
2022-04-05 12:53:42 -0400

    next

--- screens/StartScreen/StartScreen.tsx
+++ screens/StartScreen/StartScreen.tsx
@@ -20,6 +20,9 @@ export default function StartScreen( {navigation}: {navigation: any} ) {
       <Text style={styles.title}>StartScreen</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="Join" path2="The UniDrop way" path3="Go"/>
+      <TouchableOpacity key = {2} onPress={() => navigation.goBack()}>
+        <Text style={styles.button} >Last page </Text>
+      </TouchableOpacity>
       <TouchableOpacity key = {1} onPress={() => navigation.navigate('RoomID')}>
         <Text style={styles.button} > Next page </Text>
       </TouchableOpacity>
