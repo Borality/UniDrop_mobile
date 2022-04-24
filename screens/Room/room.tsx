b8152ac22ec62a2d265f2d7388e6125d5cb4dc8d
Charles Trangay <charles@nbp-152-221.nbp.ruw.rutgers.edu>
2022-04-24 17:41:58 -0400

    working on image download

--- screens/Room/room.tsx
+++ screens/Room/room.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { View, Text, Image } from "react-native";
-import {stylesMain} from "../../core-ui/components.styles";
-import {ShareRoomCard} from "../../components/shareRoomCard";
-import {encode} from "base64-arraybuffer";
+import {Button} from "react-native-elements";
+import { stylesMain } from "../../core-ui/components.styles";
+import { ShareRoomCard } from "../../components/shareRoomCard";
+import { encode } from "base64-arraybuffer";
+import * as Sharing from "expo-sharing";
+import * as FileSystem from 'expo-file-system';
 
-var ab2str = require('arraybuffer-to-string')
+var ab2str = require("arraybuffer-to-string");
 
 type RoomProps = {
 	connected: boolean;
@@ -14,13 +17,13 @@ type RoomProps = {
 };
 
 function arrayBufferToBase64(buffer: any) {
-    let binary = '';
-    let bytes = new Uint8Array(buffer);
-    let len = bytes.byteLength;
-    for (let i = 0; i < len; i++) {
-        binary += String.fromCharCode(bytes[i]);
-    }
-    return window.btoa(binary);
+	let binary = "";
+	let bytes = new Uint8Array(buffer);
+	let len = bytes.byteLength;
+	for (let i = 0; i < len; i++) {
+		binary += String.fromCharCode(bytes[i]);
+	}
+	return window.btoa(binary);
 }
 
 export const Room = ({ connected, roomId, socket }: RoomProps) => {
@@ -28,23 +31,31 @@ export const Room = ({ connected, roomId, socket }: RoomProps) => {
 	useEffect(() => {
 		socket.on("file-received", (data: ArrayBuffer, metaData: any) => {
 			setBase64String(encode(data));
-
 		});
 	}, []);
+
+
+	const shareFile = async () => {
+		const downloadPath = FileSystem.cacheDirectory + 'fileName.jpg';
+		const { uri: localUrl } = await FileSystem.downloadAsync(`data:image/png;base64,${base64String}`, downloadPath);
+		Sharing.shareAsync(localUrl);
+	}
+
 	return (
 		<>
 			{connected ? (
 				<View style={stylesMain.container}>
-					<View style = {stylesMain.mainContainer}>
-					<Text style = {stylesMain.text}>Welcome to room!</Text>
-					<Image
-						style={{ width: "100%", height: "60%"}}
-						source={{uri: `data:image/png;base64,${base64String}`}}
-					/>
+					<View style={stylesMain.mainContainer}>
+						<Text style={stylesMain.text}>Welcome to room!</Text>
+						<Image
+							style={{ width: "100%", height: "60%" }}
+							source={{ uri: `data:image/png;base64,${base64String}` }}
+						/>
+						<Button onPress = {shareFile} title = "Download"/>
 					</View>
 				</View>
 			) : (
-				<ShareRoomCard roomId={roomId}/>
+				<ShareRoomCard roomId={roomId} />
 			)}
 		</>
 	);
